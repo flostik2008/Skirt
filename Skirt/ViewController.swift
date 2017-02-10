dd1d5ce40f79629752bc19b2b53089aa05276ee0
Evgeny Vlasov <564n@Evgenys-MacBook-Pro.local>
2017-02-09 18:12:26 -0800

    UI done

--- Skirt/ViewController.swift
+++ Skirt/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gradientView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        gradientForDitailsVC(color: UIColor.clear, view: gradientView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func gradientForDitailsVC(color: UIColor, view: UIView) {
+        let gradient = CAGradientLayer()
+        
+        let gradientOrigin = view.bounds.origin
+//        let gradientWidth = UIScreen.main.bounds.width
+        let gradientWidth = view.bounds.width
+        let gradientHeight = view.bounds.height
+        let gradientSize = CGSize(width: gradientWidth, height: gradientHeight)
+        gradient.frame = CGRect(origin: gradientOrigin, size: gradientSize)
+        
+        let bottomColor = UIColor(red:0.07, green:0.07, blue:0.07, alpha:0.5)
+        gradient.colors = [color.cgColor, bottomColor.cgColor]
+        
+        view.layer.insertSublayer(gradient, at: 0)
     }
-
-
 }
-
